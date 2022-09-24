edcc11118911620a913696f01a9f396a03c8618a
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-09-24 15:46:44 +0300

    sum

--- src/index.js
+++ src/index.js
@@ -6,85 +6,79 @@ import rendCard from './js/rendCard.js';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
-  galleryContainer: document.querySelector('.gallery'),  
-  loadMoreBtn: document.querySelector('.load-more'),   
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
-   const photo = new Photo();
-    
-  refs.searchForm.addEventListener('submit', onSearch);
-  refs.loadMoreBtn.addEventListener('click', onLoadMore);
+const photo = new Photo();
 
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-    
-   function onSearch(e) {
-     e.preventDefault();    
-     refs.galleryContainer.innerHTML = '';      
-         
-     photo.query = e.currentTarget.elements.searchQuery.value.trim();
-     photo.resetPage();
-   
-     if (photo.query === '') {
-         Notify.warning('Please, fill the main field');  
-      return;
-     }   
-     
-     photo.fetchGalleryCards()   
-       .then(data => {
-         refs.galleryContainer.innerHTML = '';
-         refs.loadMoreBtn.classList.remove('is-hidden');
-   
-         if (!data.hits.length) {
-           Notify.warning(
-             `Sorry, there are no images matching your search query. Please try again.`,
-           );
-           refs.loadMoreBtn.classList.add('is-hidden');
-           return;
-         }   
-         onRenderGallery(data);
-         Notify.success(`Hooray! We found ${data.totalHits} images !!!`);
-         lightbox.refresh();
-       });
-   }
-   
-   async function onLoadMore() {  
-   photo.fetchGalleryCards().then(onScrollmake);
+function onSearch(e) {
+  e.preventDefault();
+  refs.galleryContainer.innerHTML = '';
+
+  photo.query = e.currentTarget.elements.searchQuery.value.trim();
+  photo.resetPage();
+
+  if (photo.query === '') {
+    Notify.warning('Please, fill the main field');
+    return;
   }
-   
 
-   function  onRenderGallery(data) {     
-     const markup = data.hits.map(data => rendCard(data)).join('');
-          refs.galleryContainer.insertAdjacentHTML('beforeend', markup);      
-   }
-   
+  photo.fetchGalleryCards().then(data => {
+    refs.galleryContainer.innerHTML = '';
+    refs.loadMoreBtn.classList.remove('is-hidden');
 
-  function onScrollmake(data) {
+    if (!data.hits.length) {
+      Notify.warning(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+      refs.loadMoreBtn.classList.add('is-hidden');
+      return;
+    }
     onRenderGallery(data);
- 
-   lightbox.refresh();
- 
-   const { height: cardHeight } = document
-     .querySelector(".gallery")
-     .firstElementChild.getBoundingClientRect();
-   
-   window.scrollBy({
-     top: cardHeight * 2,
-     behavior: "smooth",
-   });
- 
-   if (data.hits.length < 40 && data.hits.length > 0) {
-     refs.loadMoreBtn.classList.add('is-hidden');
-     photo.incrementPage();
-     Notiflix.info("We're sorry, but you've reached the end of search results.");
-   }
- }
-  
+    Notify.success(`Hooray! We found ${data.totalHits} images !!!`);
+    lightbox.refresh();
+  });
+
   const options = {
     rootMargin: '50px',
     root: null,
-    threshold: 0.3
+    threshold: 0.3,
   };
- 
-const observer = new IntersectionObserver(onLoadMore, options);
-observer.observe(refs.loadMoreBtn); 
- 
+
+  const observer = new IntersectionObserver(onLoadMore, options);
+  observer.observe(refs.loadMoreBtn);
+}
+
+async function onLoadMore() {
+  photo.fetchGalleryCards().then(onScrollmake);
+}
+
+function onRenderGallery(data) {
+  const markup = data.hits.map(data => rendCard(data)).join('');
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function onScrollmake(data) {
+  onRenderGallery(data);
+
+  lightbox.refresh();
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+
+  if (data.hits.length < 40 && data.hits.length > 0) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    photo.incrementPage();
+    Notiflix.info("We're sorry, but you've reached the end of search results.");
+  }
+}
